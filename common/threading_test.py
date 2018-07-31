65b5185b01044a56ae44714ab05ffe55d53f1739
flag625 <liangzhisen@outlook.com>
2018-07-31 14:01:21 +0800

    多线程类

---
+++ common/threading_test.py
@@ -0,0 +1,10 @@
+#-*- coding: utf-8 -*-
+
+class ThreadFunc(object):
+    def __init__(self, func, args, name=''):
+        self.name = name
+        self.func = func
+        self.args = args
+
+    def __call__(self):
+        self.func(*self.args)
